1f5c2bf3ea5dc4b0e975b1c9b040b43dc38ecfac
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-11 11:28:03 +0100

    Handle mobile view

--- src/Components/shared/navigationBar/NavigationBar.jsx
+++ src/Components/shared/navigationBar/NavigationBar.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
 import SideNavigationBar from "./SideNavigationBar.jsx";
 import TopNavigationBar from "./TopNavigationBar.jsx";
 
 export default function NavigationBar() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      const isMobile = window.innerWidth < 768;
+      setIsMobile(isMobile);
+    }
+
+    window.addEventListener("resize", handleResize);
+  });
+
   return (
     <div>
-      <TopNavigationBar></TopNavigationBar>
-      <SideNavigationBar></SideNavigationBar>
+      {isMobile && (
+        <>
+          <TopNavigationBar></TopNavigationBar>
+          <SideNavigationBar></SideNavigationBar>
+        </>
+      )}
     </div>
   );
 }
